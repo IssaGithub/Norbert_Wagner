593d1a26876f6b2d496c6b3536551435d1b861cb
Zaytoun <issa.zaytoun@googlemail.com>
2025-11-25 17:31:00 +0100

    base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-// Use base path only in production (GitHub Pages)
-const base = process.env.NODE_ENV === "production" ? "/NorbertWagner" : "/";
-
 export default defineConfig({
   site: "https://IssaGithub.github.io",
-  base: base,
+  base: "/NorbertWagner",
   integrations: [tailwind()],
 });
