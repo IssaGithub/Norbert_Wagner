44cf109763b4168b32a40e99131bc6fa215c0563
Zaytoun <issa.zaytoun@googlemail.com>
2025-11-25 17:21:26 +0100

    fix path for correct css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://IssaGithub.github.io",
   base: "/NorbertWagner",
-  trailingSlash: "always",
   integrations: [tailwind()],
-  build: {
-    assets: "_astro",
-  },
 });
