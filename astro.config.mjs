6128efe77798f11e182b8d70e8c922917b8219e6
Zaytoun <issa.zaytoun@googlemail.com>
2025-11-25 15:51:19 +0100

    fix css on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://IssaGithub.github.io",
   base: "/NorbertWagner",
+  trailingSlash: "always",
   integrations: [tailwind()],
+  build: {
+    assets: "_astro",
+  },
 });
