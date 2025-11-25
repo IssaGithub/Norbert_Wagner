d837dfa42f72b0239f9168836b55c7c965df1a65
Zaytoun <issa.zaytoun@googlemail.com>
2025-11-25 15:29:56 +0100

    add github site url for github oages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://github.com/IssaGithub",
+  base: "/NorbertWagner",
   integrations: [tailwind()],
 });
